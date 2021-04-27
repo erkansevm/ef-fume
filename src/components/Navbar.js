f15105e3f05e36c0711f0fbc510c255a43f9762e
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2021-04-27 22:28:26 +0300

    navbar css start

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,26 @@
 import React from 'react';
+import '../styles/navbar.scss';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-brand-title">
-          EF-FUME
-        </Link>
-      </div>
+    <nav className="navbar container">
+      <Link to="/" className="navbar-brand-links">
+        <h1 className="navbar-brand-title">EF-FUME</h1>
+      </Link>
       <div className="navbar-links">
         <h3 className="navbar-link">Shop</h3>
         <h3 className="navbar-link">About us</h3>
         <h3 className="navbar-link">Contact</h3>
       </div>
       <div className="navbar-basket-search">
-        <div>
-          <button type="button">
+        <div className="navbar-search">
+          <button className="navbar-search-button" type="button">
             <i className="fas fa-search" />
           </button>
-          <input type="text" />
+          <input type="text" placeholder="What are you looking for" />
         </div>
-        <Link to="/basket">
+        <Link className="navbar-basket" to="/basket">
           <i className="fas fa-shopping-cart" />
         </Link>
       </div>
